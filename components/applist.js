b08ffe15f6a1b14490735cdfb945ee2048ee1400
Shreya Baid <shreyabaid16@gmail.com>
2017-06-07 17:11:54 +0530

    ReactJS todo application
    react js

---
+++ components/applist.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export class ToDoAppList extends React.Component{
+	constructor(){
+		super();
+		this.remove=this.remove.bind(this);
+
+	}
+
+	remove(elem){
+		var value=elem.target.parentNode.querySelector('span').innerText;
+		this.props.remove(value);
+	}
+
+
+	render(){
+
+			var items=this.props.tasks.map((elem,i) =>{
+				return  <li key={i}> <span>{elem}</span><button onClick={this.remove}>Remove</button></li>
+
+			});
+
+		return(
+				<ul>
+				 
+					{items}
+				</ul>
+			);
+	}
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Todo} from './components/main'
+
+var tasksList=["Task1","Task2"];
+
+
+
+var tasks=localStorage.getItem('storedTasks');
+
+if(tasks){
+	tasksList=JSON.parse(tasks);
+}
+
+ReactDOM.render(
+	<Todo tasks={tasksList}/>,
+	document.getElementById('todo')
+	);
